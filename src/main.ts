6bce6913339324e205cc6c6543b4e69ec03200e1
LuckiPhoenix <huynhchihen2005@gmail.com>
2025-08-31 16:14:28 +0700

    feat(config): add helmet. closes #2

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -30,6 +31,7 @@ async function bootstrap() {
     .map((s) => s.trim())
     .filter(Boolean);
   app.enableCors({ origin: corsOrigins, credentials: true });
+  app.use(helmet());
 
   const port = Number(config.get<string>('PORT')) || 8000;
   await app.listen(port);
